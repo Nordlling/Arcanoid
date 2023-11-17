4529ed6a0a064105431e239b56f94895acdfdeca
Nordling <apodvorny@devsgroup.net>
2023-11-17 13:57:49 +0300

    Refactor LocalizationParser

--- Assets/Main/Scripts/Localization/LocalizationParser.cs
+++ Assets/Main/Scripts/Localization/LocalizationParser.cs
@@ -20,61 +20,86 @@ namespace Main.Scripts.Localization
 
 				if (languages.Count != languages.Distinct().Count())
 				{
-					Debug.LogError($"Duplicated languages found in `{sheet.Name}`. This sheet is not loaded.");
+					Debug.LogWarning($"Duplicated languages found in `{sheet.Name}`. This sheet is not loaded.");
 					continue;
 				}
 
-				for (int i = 1; i < languages.Count; i++)
-				{
-					if (!wordDictionary.ContainsKey(languages[i]))
-					{
-						wordDictionary.Add(languages[i], new Dictionary<string, string>());
-					}
-				}
-
-				for (int i = 1; i < lines.Count; i++)
-				{
-					List<string> columns = GetColumns(lines[i]);
-					string key = columns[0];
-
-					if (key == "")
-					{
-						continue;
-					}
-
-					if (keys.Contains(key))
-					{
-						Debug.LogError($"Duplicated key `{key}` found in `{sheet.Name}`. This key is not loaded.");
-						continue;
-					}
-
-					keys.Add(key);
-
-					for (int j = 1; j < languages.Count; j++)
-					{
-						if (wordDictionary[languages[j]].ContainsKey(key))
-						{
-							Debug.LogError($"Duplicated key `{key}` in `{sheet.Name}`.");
-						}
-						else
-						{
-							wordDictionary[languages[j]].Add(key, columns[j]);
-						}
-					}
-				}
+				FillLanguages(languages, wordDictionary);
+				FillWords(lines, keys, sheet, languages, wordDictionary);
 			}
 
 			return wordDictionary;
 		}
 
-		private static List<string> GetLines(string text)
+        private static void FillLanguages(List<string> languages, Dictionary<string, Dictionary<string, string>> wordDictionary)
+        {
+	        for (int i = 1; i < languages.Count; i++)
+	        {
+		        if (!wordDictionary.ContainsKey(languages[i]))
+		        {
+			        wordDictionary.Add(languages[i], new Dictionary<string, string>());
+		        }
+	        }
+        }
+
+        private static void FillWords(List<string> lines, List<string> keys, Sheet sheet, List<string> languages,
+	        Dictionary<string, Dictionary<string, string>> wordDictionary)
+        {
+	        for (int i = 1; i < lines.Count; i++)
+	        {
+		        List<string> columns = GetColumns(lines[i]);
+		        string key = columns[0];
+
+		        if (!ValidateKey(keys, sheet, key))
+		        {
+			        continue;
+		        }
+
+		        keys.Add(key);
+
+		        for (int j = 1; j < languages.Count; j++)
+		        {
+			        if (wordDictionary[languages[j]].ContainsKey(key))
+			        {
+				        Debug.LogWarning($"Duplicated key `{key}` in `{sheet.Name}`.");
+				        continue;
+			        }
+
+			        wordDictionary[languages[j]].Add(key, columns[j]);
+		        }
+	        }
+        }
+
+        private static bool ValidateKey(List<string> keys, Sheet sheet, string key)
+        {
+	        if (string.IsNullOrEmpty(key))
+	        {
+		        Debug.LogWarning($"Empty key in `{sheet.Name}`");
+		        return false;
+	        }
+
+	        if (keys.Contains(key))
+	        {
+		        Debug.LogWarning($"Duplicated key `{key}` found in `{sheet.Name}`. This key is not loaded.");
+		        return false;
+	        }
+
+	        return true;
+        }
+
+        private static List<string> GetLines(string text)
 		{
 			string _text = text.Replace("\r\n", "\n").Replace("\"\"", "[_quote_]");
 			MatchCollection matches = Regex.Matches(_text, "\"[\\s\\S]+?\"");
 
 			foreach (Match match in matches)
 			{
-				_text = _text.Replace(match.Value, match.Value.Replace("\"", null).Replace(",", "[_comma_]").Replace("\n", "[_newline_]"));
+				_text = _text.Replace(
+					match.Value,
+					match.Value
+						.Replace("\"", null)
+						.Replace(",", "[_comma_]")
+						.Replace("\n", "[_newline_]"));
 			}
 
 			return _text.Split('\n').Where(i => i != "").ToList();
@@ -82,7 +107,14 @@ namespace Main.Scripts.Localization
 
 		private static List<string> GetColumns(string line)
 		{
-			return line.Split(',').Select(j => j.Trim()).Select(j => j.Replace("[_quote_]", "\"").Replace("[_comma_]", ",").Replace("[_newline_]", "\n")).ToList();
+			return line
+				.Split(',')
+				.Select(j => j.Trim())
+				.Select(j => j
+					.Replace("[_quote_]", "\"")
+					.Replace("[_comma_]", ",")
+					.Replace("[_newline_]", "\n"))
+				.ToList();
 		}
 	}
 }
